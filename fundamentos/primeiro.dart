35140a43f669714f08eae77b153d7babbd06f5ce
Bernardo <bernardo@iMac-Pro-de-Bernardo.local>
2021-09-14 15:18:24 -0400

    feat: finalizando introdução e sobre variaveis

---
+++ fundamentos/primeiro.dart
@@ -0,0 +1,16 @@
+// Main é a porta de entrada do dart.
+main() {
+  print("Olá mundo!");
+  if (true) {
+    print("Olá mundo!");
+  }
+
+  // Sentenças vazias
+  {
+    ;
+    ;
+    ;
+  }
+
+  print('Fim!');
+}
