b1a7be0bc1912dca67dfb1b4a0af41c136a81129
Bernardo <bernardo@iMac-Pro-de-Bernardo.local>
2021-09-14 22:32:29 -0400

    feat: finalizando aula 8 sobre operadores aritmetico e lógicos

---
+++ fundamentos/operadores_1.dart
@@ -0,0 +1,26 @@
+main() {
+  // Aritméticos (operadors binários/infix)
+  int a = 7;
+  int b = 3;
+
+  int resultado = a + b;
+
+  print(resultado);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+  print(33 % 2);
+  print(32 % 4);
+
+  print(a + b * a - b / a);
+
+  // Operadores Lógicos
+  bool ehFragil = true;
+  bool ehCaro = true;
+
+  print(ehCaro & ehFragil); //AND => E
+  print(ehFragil | ehCaro); //OR => OU
+  print(ehFragil ^ ehCaro); //XOR => OU Exclusivo
+  print(!ehFragil); //(NOT) => Unário/Prefix
+}
