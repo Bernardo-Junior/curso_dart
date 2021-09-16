12d1bfbcd1830d5bfe0233d5b01814129a2a2ad5
Bernardo <bernardo@iMac-Pro-de-Bernardo.local>
2021-09-16 16:45:15 -0400

    feat: aula sobre criação de get e set

--- oo/model/carro.dart
+++ oo/model/carro.dart
@@ -1,34 +1,44 @@
 class Carro {
   final int? velocidadeMaxima;
-  int velocidadeAtual;
+  int _velocidadeAtual;
 
-  Carro([this.velocidadeAtual = 0, this.velocidadeMaxima]);
+  Carro([this._velocidadeAtual = 0, this.velocidadeMaxima]);
+
+  int get velocidadeAtual {
+    return this._velocidadeAtual;
+  }
+
+  void set velocidadeAtual(int novaVelocidade) {
+    if (novaVelocidade <= velocidadeMaxima! && novaVelocidade >= 0) {
+      this._velocidadeAtual = novaVelocidade;
+    }
+  }
 
   int? acelerar() {
-    if (this.velocidadeAtual == velocidadeMaxima ||
-        (this.velocidadeAtual + 5) > this.velocidadeMaxima!) {
+    if (this._velocidadeAtual == velocidadeMaxima ||
+        (this._velocidadeAtual + 5) > this.velocidadeMaxima!) {
       print("\n\nO carro está na sua velocidade máxima!\n\n");
-      return this.velocidadeAtual;
+      return this._velocidadeAtual;
     }
 
     print("\n\nAcelerando...");
-    int soma = this.velocidadeAtual += 5;
+    int soma = this._velocidadeAtual += 5;
     return soma;
   }
 
   int? freiar() {
-    if (this.velocidadeAtual == 0 || (this.velocidadeAtual - 5) < 0) {
+    if (this._velocidadeAtual == 0 || (this._velocidadeAtual - 5) < 0) {
       print("\n\nO carro está na sua velocidade mínima!\n\n");
-      return this.velocidadeAtual;
+      return this._velocidadeAtual;
     }
 
     print("\n\nFreiando...");
-    int subtracao = this.velocidadeAtual -= 5;
+    int subtracao = this._velocidadeAtual -= 5;
     return subtracao;
   }
 
   bool? estaNoLimite() {
-    if (this.velocidadeAtual == this.velocidadeMaxima) {
+    if (this._velocidadeAtual == this.velocidadeMaxima) {
       return true;
     }
     return false;
